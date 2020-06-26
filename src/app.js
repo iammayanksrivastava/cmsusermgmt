bbb003fc7c0345d3f3449995cbd30b6ab3357309
Mayank Srivastava <mayank.srivastav84@gmail.com>
2020-06-26 20:38:13 +0200

    Added CORS

--- src/app.js
+++ src/app.js
@@ -3,10 +3,13 @@ const port = process.env.PORT
 const userRouter = require('./routers/user')
 require('./db/db')
 
-const app = express()
+const app = express();
+const cors = require('cors')
 
 app.use(express.json())
 app.use(userRouter)
+
+app.use(cors())
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
