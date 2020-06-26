e406d0f0939fe53e4cc9f6becd3bf20f2d3bde95
Mayank Srivastava <mayank.srivastav84@gmail.com>
2020-06-26 19:25:18 +0200

    initial code

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: true
-})
+// mongoose.connect(process.env.MONGODB_URL, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: true
+// })
+
+mongoose
+  .connect('mongodb+srv://mayank:residency18@cluster0-pu5tf.azure.mongodb.net/cmsusermgmt', {useNewUrlParser: true})
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
